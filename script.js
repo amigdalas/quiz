4fd89d8151bbca683c7ac6789ff9681fa29d463d
amigdalas <90859449+amigdalas@users.noreply.github.com>
2022-02-18 00:50:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ const ChangeQuestion = async (n) => {
 
   if (perguntas[n] == undefined) (counter = 0), (n = 0);
 
-  if (  n < maximo ) //(n != 0 && document.getElementById("perguntasBox").innerHTML != respostas[3]) &&
+  if (  document.getElementById("perguntasBox").innerHTML != respostas[3] && n < maximo ) //(n != 0 && document.getElementById("perguntasBox").innerHTML != respostas[3]) &&
     return (document.getElementById("perguntasBox").innerHTML = respostas[3]), bgColor = "#00003B";
 
   if (n < maximo) for (let i = 0; i < perguntas2.length; i++) { 
